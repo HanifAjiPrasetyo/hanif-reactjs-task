dbaf5c2c03f0daebd9280a164e18a5f1ac6de4e5
HanifAjiPrasetyo <hanifaji007@gmail.com>
2024-05-02 20:45:19 +0700

    task-1-finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Task1 from "./components/task1";
+
 function App() {
   return (
-    <>
-      <div className=""></div>
-    </>
+    <div className="App">
+      <div className="min-h-screen flex items-center justify-center">
+        <Task1 title="Pemandangan" img="https://images.tokopedia.net/img/cache/700/VqbcmM/2023/7/29/56e260fd-93bd-4055-baeb-ed502f6e1272.jpg" />
+      </div>
+    </div>
   );
 }
 
